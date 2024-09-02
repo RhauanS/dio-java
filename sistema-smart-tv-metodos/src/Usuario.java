1faf96d3f6d5e8da3b4414801dfdfebefe7b46d3
Rhauan <santosrhauan@gmail.com>
2024-09-02 15:18:50 -0300

    criação de classe

--- sistema-smart-tv-metodos/src/Usuario.java
+++ sistema-smart-tv-metodos/src/Usuario.java
@@ -1,10 +1,26 @@
 public class Usuario {
    public static void main(String[] args) {
-    SmartTV smartTV = new SmartTV();
+        SmartTV compotamento = new SmartTV();   
 
-        System.out.println("TV Ligada ? " + smartTV.ligada);
-        System.out.println("Canal atual: " + smartTV.canal);
-        System.out.println("Volume atual: " + smartTV.volume);
+       
 
-   }
+        System.out.println("TV Ligada ? " + compotamento.ligada);
+        System.out.println("Canal atual: " + compotamento.canal);
+        System.out.println("Volume atual: " + compotamento.volume);
+
+        for(int i = 0; i < 3; i++){
+            compotamento.aumentarCanal();
+            compotamento.aumentarVolume();
+            compotamento.ligar();
+            
+        }
+
+        System.out.println("Status -> TV Ligada ? " + compotamento.ligada);
+        System.out.println("Status -> Canal atual: " + compotamento.canal);
+        System.out.println("Status -> Volume atual: " + compotamento.volume);
+
+
+
+
+        }
 }
