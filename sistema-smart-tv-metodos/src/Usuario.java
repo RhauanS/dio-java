b817bb255e33a22d6aefa0dbf0c5f23c60d680cb
Rhauan <santosrhauan@gmail.com>
2024-09-02 14:29:26 -0300

    criação do app

---
+++ sistema-smart-tv-metodos/src/Usuario.java
@@ -0,0 +1,10 @@
+public class Usuario {
+   public static void main(String[] args) {
+    SmartTV smartTV = new SmartTV();
+
+        System.out.println("TV Ligada ? " + smartTV.ligada);
+        System.out.println("Canal atual: " + smartTV.canal);
+        System.out.println("Volume atual: " + smartTV.volume);
+
+   }
+}
