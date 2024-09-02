88d93ee0d7f450103122e35b25377b667864c24f
Rhauan <santosrhauan@gmail.com>
2024-09-02 02:38:57 -0300

    introdução do tipo de variaveis

--- Tipos-variaveis/src/App.java
+++ Tipos-variaveis/src/App.java
@@ -1,5 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        //tipos de variavel
+        short idade = 18;
+        int ano = 2024;
+        String nome = "Rhauan";
+        byte anoDeVidadoCachorro = 05;
+
+
+
+
+        sc.close();
+
     }
 }
