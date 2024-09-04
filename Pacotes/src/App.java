677d1d8241f0d56e09a2d905a58066fde53d3ec6
Rhauan <santosrhauan@gmail.com>
2024-09-04 01:08:25 -0300

    criação de app pacotes para estudos do package

---
+++ Pacotes/src/App.java
@@ -0,0 +1,15 @@
+/*import Videos.Documentario;
+import Videos.Filme;
+import Videos.Novela;*/
+
+import br.com.devflix.Videos.*;
+import br.com.devflix.gui.Janela;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+     Filme  filme = new Filme();
+     Novela novela = new Novela();
+     Documentario documentario = new Documentario();   
+     Janela janela = new Janela();   
+    }
+}
---
+++ Pacotes/src/br/com/devflix/Videos/Documentario.java
@@ -0,0 +1,5 @@
+package br.com.devflix.Videos;
+
+public class Documentario {
+    
+}
